87e83f3ed273f1068b739d32a0d21e8490b7ae02
Ali Hassnain <ali1397hasnain@gmail.com>
2024-05-02 17:31:48 +0500

    git ignore

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,21 +1,19 @@
 const User = require("../models/userModel")
+const asyncHandler = require("express-async-handler")
 
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
 
     const email = req.body.email;
 
-    const findUser = await User.findOne(email);
+    const findUser = await User.findOne({ email: email });
 
     if (!findUser) {
         const newUser = await User.create(req.body);
         res.json(newUser)
     }
     else {
-        res.json({
-            message: "User Exists",
-            success: "false"
-        })
+        throw new Error("User Already Exists")
     }
 
-}
+})
 module.exports = createUser;
