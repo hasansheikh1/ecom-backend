829977623f984ae73bc4dc2fd013926cebbfd4b6
Ali Hassnain <ali1397hasnain@gmail.com>
2024-05-03 17:24:16 +0500

    usercontllr

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,3 +1,4 @@
+const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel")
 const asyncHandler = require("express-async-handler")
 
@@ -16,4 +17,52 @@ const createUser = asyncHandler(async (req, res) => {
     }
 
 })
-module.exports = createUser;
+
+const loginUserCtrl = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    // console.log(email, password)
+    const findUser = await User.findOne({ email })
+
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+        res.json({
+            _id: findUser?._id,
+            firstname: findUser?.firstname,
+            email: findUser?.email,
+            token: generateToken(findUser?._id)
+        })
+    }
+    else {
+        throw new Error("Invalid Credentials")
+    }
+
+})
+
+const getAllUsers = asyncHandler(async (req, res) => {
+    try {
+
+        const getUsers = await User.find();
+        res.json(getUsers)
+    }
+    catch (error) {
+        throw new Error("Error", error)
+    }
+
+})
+
+const getUser = asyncHandler(async (req, res) => {
+
+    const { id } = req.params;
+    try {
+        const getUser = await User.findById(id)
+        res.json({
+            success: true,
+            getUser,
+        })
+    }
+    catch (error) {
+        console.log("Error", error)
+    }
+    console.log(id)
+})
+
+module.exports = { createUser, loginUserCtrl, getAllUsers, getUser };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const createUser = require('../controller/userCtrl');
+const { createUser, loginUserCtrl, getAllUsers, getUser } = require('../controller/userCtrl');
 const router = express.Router();
 
 
-router.post('/register', createUser)
+router.post('/register', createUser);
+router.post('/login', loginUserCtrl);
+router.get('/all-users', getAllUsers);
+router.get('/:id', getUser);
 
 module.exports = router;
