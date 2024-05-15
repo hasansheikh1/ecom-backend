8484686a4497ac7cf85555e0b9848f7d15891d2c
Ali Hassnain <ali1397hasnain@gmail.com>
2024-05-15 17:12:27 +0500

    changes

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -5,7 +5,7 @@ const slugify = require("slugify")
 const createProduct = asyncHandler(async (req, res) => {
 
     try {
-        if(req.body.title){
+        if (req.body.title) {
             req.body.slug = slugify(req.body.title)
 
         }
@@ -17,47 +17,65 @@ const createProduct = asyncHandler(async (req, res) => {
 
 })
 
-const updateProduct = asyncHandler (async(req,res)=>{
-const id = req.params;
-console.log("id ",id)
+const updateProduct = asyncHandler(async (req, res) => {
+    const id = req.params;
+    console.log("id ", id)
     try {
-        
-        if(req.body.title){
-            req.body.slug = slugify (req.body.title)
+
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title)
         }
-        const updateProduct = await Product.findOneAndUpdate({id},
+        const updateProduct = await Product.findOneAndUpdate({ id },
             req.body,
-            {new:true}
+            { new: true }
         )
         res.json(updateProduct)
     } catch (error) {
-            throw new Error(error)
+        throw new Error(error)
     }
 })
 
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    try {
 
-const getaProduct = asyncHandler(async(req,res)=>{
-    const {id}= req.params;
-    try{
+        const deleteProduct = await Product.findOneAndDelete({ _id: id });
+        console.log("delete product", deleteProduct)
+        if (!deleteProduct) {
+            // If no product was found with that ID, return a 404 status
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.json(deleteProduct)
+
+    } catch (error) {
+        throw new Error(error)
+    }
+
+})
+
+const getaProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    try {
         const findProduct = await Product.findById(id);
         res.json(findProduct)
-    }   
-    catch(error){
+    }
+    catch (error) {
         throw new Error(error);
     }
 })
 
-const getAllProducts = asyncHandler(async(req,res)=>{
+const getAllProducts = asyncHandler(async (req, res) => {
 
     try {
-        const getAllProducts = await Product.find();    
+        const getAllProducts = await Product.find();
         res.json(getAllProducts);
 
     } catch (error) {
-            throw new Error(error)
-    }   
-    
+        throw new Error(error)
+    }
+
 
 })
 
-module.exports = { createProduct,updateProduct,getaProduct,getAllProducts }
+module.exports = { createProduct, updateProduct, deleteProduct, getaProduct, getAllProducts }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProduct, getaProduct, getAllProducts, updateProduct } = require('../controller/productCtrl');
+const { createProduct, getaProduct, getAllProducts, updateProduct, deleteProduct } = require('../controller/productCtrl');
 
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', createProduct)
 router.get('/:id', getaProduct)
 router.get('/', getAllProducts)
+router.delete('/:id', deleteProduct)
 router.put('/:id', updateProduct)
 module.exports = router;
